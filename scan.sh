4a4a38309e31e1d3c9c6afb84294f72472062f90
Michal Špaček <mail@michalspacek.cz>
2014-06-19 20:26:16 +0200

    Go back to higher speed and don't parse RIPE data if these are already parsed

--- scan.sh
+++ scan.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 scanport() {
-	[ ! -f data/czip$1 ] && sudo masscan -p$1 --conf data/ripe.cz.conf --rate=5000 --output-format greppable --output-file data/massoutput$1 --excludefile exclude.txt --wait 30 \
+	[ ! -f data/czip$1 ] && sudo masscan -p$1 --conf data/ripe.cz.conf --rate=10000 --output-format greppable --output-file data/massoutput$1 --excludefile exclude.txt --wait 30 \
 	&& grep "Ports: $1" data/massoutput$1 | grep -oP "(?<=Host: )\d+\.\d+.\d+.\d+" > data/czip$1
-	ssltest.py --input data/czip$1 --port $1 --json data$1.json --logfile data/results.txt --timeout 30 --threads 500
+	ssltest.py --input data/czip$1 --port $1 --json data$1.json --logfile data/results.txt --timeout 5 --threads 1000
 }
 
 mkdir -p data
@@ -11,6 +11,6 @@ cd data
 [ ! -f ripe.db.inetnum ] && wget ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inetnum.gz && gunzip ripe.db.inetnum.gz
 cd ..
 
-./parse-ripedb.py data/ripe.db.inetnum cz data/ripe.cz.conf
+[ ! -f data/ripe.cz.conf ] && ./parse-ripedb.py data/ripe.db.inetnum cz data/ripe.cz.conf
 
 scanport 443 && scanport 465 && scanport 993 && scanport 995
