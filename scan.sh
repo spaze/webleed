1e0651ce816af0fd058669289dd58621e33fdfa1
Michal Špaček <mail@michalspacek.cz>
2014-06-23 18:26:34 +0200

    Half the threads and twice the timeout for the Heartbleed scanner

--- scan.sh
+++ scan.sh
@@ -3,7 +3,7 @@
 scanport() {
 	[ ! -f data/czip$1 ] && sudo masscan -p$1 --conf data/ripe.cz.conf --rate=10000 --output-format greppable --output-file data/massoutput$1 --excludefile exclude.txt --wait 30 \
 	&& grep "Ports: $1" data/massoutput$1 | grep -oP "(?<=Host: )\d+\.\d+.\d+.\d+" > data/czip$1
-	ssltest.py --input data/czip$1 --port $1 --json data$1.json --logfile data/results.txt --timeout 5 --threads 1000
+	ssltest.py --input data/czip$1 --port $1 --json data$1.json --logfile data/results.txt --timeout 10 --threads 500
 }
 
 mkdir -p data
