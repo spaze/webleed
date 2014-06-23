74a3aa093fd83bc357c78a5fea6fe69fa2e0c07c
Michal Špaček <mail@michalspacek.cz>
2014-06-23 15:44:57 +0200

    Parse results and create .tar.bz2 with SQL queries to insert into local database

---
+++ parse.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+I=`head -1 ./scanid`
+for P in 443 465 993 995; do
+	INITIATED=`head -1 ./data/massoutput$P | grep -oP "(?<= initiated ).*"`
+	START=`date -d "$INITIATED" +"%Y-%m-%d %H:%M:%S+00"`
+	./parse-data.py --scanid $I --json data$P.json --masscan-conf data/ripe.cz.conf --port $P --ip 95.168.205.151 --start "$START" --total `head -1 ./data/total` > data$I.sql
+	echo "$P parsed"
+	I=$((I+1))
+done;
+echo $I > ./scanid
+
+START=`date -d "$INITIATED" +"%Y-%m-%d"`
+tar cjf data$START.tar.bz2 data*.sql
