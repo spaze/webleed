d3d972904aac2140cf8cd68cd768fea479da40ab
Michal Špaček <mail@michalspacek.cz>
2015-02-16 01:40:00 +0100

    Read the scanner IP from file called ip

--- parse.sh
+++ parse.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+IP=`head -1 ./ip`
 I=`head -1 ./scanid`
 for P in 443 465 993 995; do
 	INITIATED=`head -1 ./data/massoutput$P | grep -oP "(?<= initiated ).*"`
 	START=`date -d "$INITIATED" +"%Y-%m-%d %H:%M:%S+00"`
-	./parse-data.py --scanid $I --json data$P.json --masscan-conf data/ripe.cz.conf --port $P --ip 95.168.205.151 --start "$START" --total `head -1 ./data/total` > data$I.sql
+	./parse-data.py --scanid $I --json data$P.json --masscan-conf data/ripe.cz.conf --port $P --ip $IP --start "$START" --total `head -1 ./data/total` > data$I.sql
 	echo "$P parsed"
 	I=$((I+1))
 done;
